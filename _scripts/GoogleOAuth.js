46bba2aab445e14260c4d4da893d8c2a139a4cd3
solidcoreconsulting <33646496+solidcoreconsulting@users.noreply.github.com>
2017-11-28 23:44:45 -0500

    added redirect page

--- _scripts/GoogleOAuth.js
+++ _scripts/GoogleOAuth.js
@@ -2,13 +2,20 @@ var profile;
 
 function onSignIn(googleUser) {
 	profile = googleUser.getBasicProfile();
+
+	// .then{
+	// 	window.location = 'test.html';
+	// };
 }
 
 function signOut() {
 	var auth2 = gapi.auth2.getAuthInstance();
 	auth2.signOut().then(function () {
-	console.log('User signed out.');
+		console.log('User signed out.');
 	});
+	// auth2.signOut().then(function () {
+	//
+	// });
 }
 
 function print(){
@@ -16,99 +23,4 @@ function print(){
 	document.write('Name: ' + profile.getName()+  "<br>");
 	document.write('Image URL: ' + profile.getImageUrl()+  "<br>");
 	document.write('Email: ' + profile.getEmail()+  "<br>"); // This is null if the 'email' scope is not present.
-}
-
-
-
-
-
-
-/**
-*  On load, called to load the auth2 library and API client library.
-*/
-function handleClientLoad() {
-	gapi.load('client:auth2', initClient);
-}
-
-/**
-*  Initializes the API client library and sets up sign-in state
-*  listeners.
-*/
-function initClient() {
-	gapi.client.init({
-	apiKey: API_KEY,
-	clientId: CLIENT_ID,
-	discoveryDocs: DISCOVERY_DOCS,
-	scope: SCOPES
-}).then(function () {
-  // Listen for sign-in state changes.
-  gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-  // Handle the initial sign-in state.
-  updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-  authorizeButton.onclick = handleAuthClick;
-  signoutButton.onclick = handleSignoutClick;
-});
-}
-
-/**
-*  Called when the signed in status changes, to update the UI
-*  appropriately. After a sign-in, the API is called.
-*/
-function updateSigninStatus(isSignedIn) {
-if (isSignedIn) {
-  authorizeButton.style.display = 'none';
-  signoutButton.style.display = 'block';
-  listFiles();
-} else {
-  authorizeButton.style.display = 'block';
-  signoutButton.style.display = 'none';
-}
-}
-
-/**
-*  Sign in the user upon button click.
-*/
-function handleAuthClick(event) {
-gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
-*  Sign out the user upon button click.
-*/
-function handleSignoutClick(event) {
-gapi.auth2.getAuthInstance().signOut();
-}
-
-/**
-* Append a pre element to the body containing the given message
-* as its text node. Used to display the results of the API call.
-*
-* @param {string} message Text to be placed in pre element.
-*/
-function appendPre(message) {
-var pre = document.getElementById('content');
-var textContent = document.createTextNode(message + '\n');
-pre.appendChild(textContent);
-}
-
-/**
-* Print files.
-*/
-function listFiles() {
-gapi.client.drive.files.list({
-  'pageSize': 10,
-  'fields': "nextPageToken, files(id, name)"
-}).then(function(response) {
-  appendPre('Files:');
-  var files = response.result.files;
-  if (files && files.length > 0) {
-	for (var i = 0; i < files.length; i++) {
-	  var file = files[i];
-	  appendPre(file.name + ' (' + file.id + ')');
-	}
-  } else {
-	appendPre('No files found.');
-  }
-});
 }
